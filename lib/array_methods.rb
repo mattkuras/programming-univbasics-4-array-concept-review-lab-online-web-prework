30b9a3b4cd99f29e262173d10712047bf6d73a58
Matt Kuras <mattkuras@gmail.com>
2020-02-14 00:55:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -24,5 +24,16 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
+    counter = 0 
+    min_value = array[0]
+  while counter < array.length do 
+    
+    if array[counter] < min_value
+      min_value = array[counter]
   
+  end
+  counter += 1 
+  end
+  min_value 
+end
 end
